c7d8ffeeaf8ba2935d0bf30e4a58ff5962c3f7da
guohuanw <guohuan@gmail.com>
2022-10-11 10:16:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Patents New Healthy Diner') 
 streamlit.header('Breakfast Menu')
-streamlit.text('\N{U+1F347} Omega 3 & Blueberry Oatmeal')
+streamlit.text('\N{bouquet} Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
